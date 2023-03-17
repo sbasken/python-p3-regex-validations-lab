fe0fe5d5eace222db567fe5e059ea54df225b9d1
Saki <sbasken@gmail.com>
2023-03-16 19:37:40 -0700

    pass requirements - verified on regex 101. Issues with the tests

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"[\w\s\d\'\-]+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\(?\d{3}\)?[-\s]?\d{3}-?\d{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[\d\w]\.\w\d]+@[a-z]+.com"
 email_regex = re.compile(email_address)
